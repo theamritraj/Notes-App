fbec05a73fd709518030fe3fb73b945a9e316623
Amrit Raj <141901677+theamritraj@users.noreply.github.com>
2025-01-23 20:41:07 +0530

    code

--- Notes App/src/components/CreateNote.tsx
+++ Notes App/src/components/CreateNote.tsx
@@ -3,6 +3,12 @@ import { InputBase, Box, Button } from '@mui/material';
 import './CreateNote.css'; // Import the CSS file
 
 const CreateNote: React.FC = () => {
+  const btn = {
+    backgroundColor: '#f5f5f5',
+    '&:hover': {
+      backgroundColor: '#e0e0e0',
+    },
+  }
   return (
     <Box className="create-note-container">
       <InputBase className="input-title" placeholder='Title' />
@@ -13,7 +19,7 @@ const CreateNote: React.FC = () => {
 
       <input type="color" className="color-input" defaultValue={'#f5f5f5'} />
 
-      <Button variant='outlined' className="create-button">Create</Button>
+      <Button variant='outlined' className="create-button" style={btn}>Create</Button>
     
     </Box>
     
